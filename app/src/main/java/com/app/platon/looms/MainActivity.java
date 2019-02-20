f26698e83016f2176c98092ee794e356cfbfac33
DarshReddy <33743239+DarshReddy@users.noreply.github.com>
2019-02-20 17:28:47 +0530

    first commit

--- app/src/main/java/com/app/platon/looms/MainActivity.java
+++ app/src/main/java/com/app/platon/looms/MainActivity.java
@@ -20,10 +20,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab =findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_wishlist) {
             return true;
         }
 
